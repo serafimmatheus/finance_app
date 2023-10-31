c61bb6dbaf76800243255c7b4a9bc4b6b6806504
serafim <matheus18serafim@gmail.com>
2023-10-31 08:42:26 -0300

    fix: remove routes for in app.route.ts

--- src/app.ts
+++ src/app.ts
@@ -1,51 +1,13 @@
-import express from 'express'
-import { prisma } from './lib/prisma'
-import zod from 'zod'
-import { hash } from 'bcrypt'
+import express, { Request, Response } from 'express'
+import { initApp } from './routes/app.routes'
+import { errorHandler } from './err/errorHandler'
 
 export const app = express()
 
 app.use(express.json())
 
-app.get('/api/users', async (req, res) => {
-    const users = await prisma.user.findMany()
+initApp(app)
 
-    res.status(200).json(users)
-})
-
-app.post('/api/users', async (req, res) => {
-    const schemaBody = zod.object({
-        firstName: zod.string(),
-        lastName: zod.string(),
-        email: zod.string().email(),
-        passwordHash: zod
-            .string()
-            .min(8, { message: 'Minimo 8 caracteres' })
-            .max(30, { message: 'Maximo 30 caracteres' }),
-    })
-
-    try {
-        const { email, firstName, lastName, passwordHash } = schemaBody.parse(
-            req.body
-        )
-
-        const password = await hash(passwordHash, 10)
-
-        const newUser = {
-            email,
-            firstName,
-            lastName,
-            password,
-        }
-
-        const user = await prisma.user.create({
-            data: newUser,
-        })
-
-        res.status(201).json(user)
-    } catch (error) {
-        res.status(400).json({
-            error,
-        })
-    }
+app.use((err: Error, _: Request, res: Response) => {
+    return errorHandler(err, res)
 })
