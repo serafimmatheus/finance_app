148abe881a55332b0623b27656b7de41f3db63ed
serafim <matheus18serafim@gmail.com>
2023-10-31 08:43:24 -0300

    feat: error for app

---
+++ src/err/errorHandler.ts
@@ -0,0 +1,27 @@
+import { Response } from 'express'
+
+class ErrorHandler extends Error {
+  statusCode: number
+  message: string
+
+  constructor(statusCode: number, message: string) {
+    super()
+    this.statusCode = statusCode
+    this.message = message
+  }
+}
+
+const errorHandler = (err: Error, res: Response) => {
+  if (err instanceof ErrorHandler) {
+    return res.status(err.statusCode).json({
+      message: err.message,
+      statusCode: err.statusCode,
+    })
+  }
+
+  return res.status(500).json({
+    message: 'Internal Server Error',
+  })
+}
+
+export { ErrorHandler, errorHandler }
